808a9e630ab021cc94c7bdc657e4133f9b2fbc47
ftao <Filia.Tao@gmail.com>
2011-04-06 10:06:05 +0800

    check ezengage_config[open]

--- ezengage.php
+++ ezengage.php
@@ -2,13 +2,15 @@
 define('SCR', 'ezengage');
 require_once ('global.php');
 require_once(D_P.'data/bbscache/ezengage_config.php');
-if($_GET['mod'] == 'token'){
-    require_once(R_P.'hack/ezengage/token.inc.php');
-}
-else if($_GET['mod'] == 'bind'){
-    require_once(R_P.'hack/ezengage/bind.inc.php');
-}
-else if($_GET['mod'] == 'js'){
-    require_once(R_P.'hack/ezengage/js.inc.php');
+if($ezengage_config['open']){
+    if($_GET['mod'] == 'token'){
+        require_once(R_P.'hack/ezengage/token.inc.php');
+    }
+    else if($_GET['mod'] == 'bind'){
+        require_once(R_P.'hack/ezengage/bind.inc.php');
+    }
+    else if($_GET['mod'] == 'js'){
+        require_once(R_P.'hack/ezengage/js.inc.php');
+    }
 }
 ?>
